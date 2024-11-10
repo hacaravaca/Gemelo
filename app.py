92fe945d1975e0f56b654ae6e1e26055fbc49ff5
hacaravaca <hacaravaca@gmail.com>
2024-11-10 17:47:17 -0600

    Cambios al python para modifycar el formato del json

--- app.py
+++ app.py
@@ -1,24 +1,19 @@
-from flask import Flask, send_file, jsonify
+from flask import Flask, jsonify, send_file
+import json
 
 app = Flask(__name__)
 
-# Ruta para obtener el archivo .obj
 @app.route('/get_obj', methods=['GET'])
 def get_obj():
-    try:
-        return send_file('cube.obj', mimetype='application/octet-stream', as_attachment=True)
-    except Exception as e:
-        return str(e), 500
+    return send_file('cube.obj', mimetype='application/octet-stream')
 
-# Ruta para obtener el archivo .json
 @app.route('/get_json', methods=['GET'])
 def get_json():
-    try:
-        with open('J.json', 'r') as json_file:
-            data = json_file.read()
-        return jsonify(data)
-    except Exception as e:
-        return str(e), 500
+    # Leer el contenido del archivo J.json
+    with open('J.json', 'r') as json_file:
+        data = json.load(json_file)  # Cargar el JSON desde el archivo
+
+    return jsonify(data)  # Devolver el contenido como JSON
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
